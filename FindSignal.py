5e5f1d693e049f681b4139e500bfd7f3cf4ae729
Atlantik <atlantiklimani1@gmail.com>
2023-04-18 17:21:37 +0200

    Update FindSignal.py

--- FindSignal.py
+++ FindSignal.py
@@ -1,10 +1,20 @@
 from PIL import Image
 import numpy as np
 import math as math
+import os
+from sklearn.svm import SVC
+import pandas as pd
 
+def createArr(val, limit):
+    rez = []
+    for i in range(0, limit):
+        rez.append(val)
+    
+    return rez
 
-def genPixelAvg(filename,size):    
-    img = Image.open('./Assets/'+filename)
+def genPixelAvg(filename,size,file_path):    
+    # img = Image.open('./Assets/'+filename)
+    img = Image.open(file_path+filename)
     img = img.resize(size)
     
     if(img.mode == 'RGB' or img.mode == 'RGBA') : 
@@ -36,11 +46,13 @@ def genCells(m):
              rez[i][j] = cell    
     return rez
 
-def genHOG(m):
+def genHOG(file_name, file_path):
+    m = genCells(genPixelAvg(file_name,(32,64), file_path))
     rez = []
     for i in range(0, len(m)):
         for j in range(0, len(m[0])):
-            h = np.zeros(9)
+            # h = np.zeros(9)
+            h = createArr(val=0, limit=9)
             for k in range(0, 4):
                 for l in range(0,4):
                     # print("i: ",i,", j: ",j,", k: ",k,", l: ",l)
@@ -83,14 +95,40 @@ def genHOG(m):
                     second_bin_degree = second_bin*20
                    
                     if(first_bin < 8):
-                        h[first_bin] += (second_bin_degree-orientation)/20 * magnitude
-                        h[second_bin] += (orientation - first_bin_degree)/20 * magnitude
+                        h[first_bin] += math.floor((second_bin_degree-orientation)/20 * magnitude)
+                        h[second_bin] += math.floor((orientation - first_bin_degree)/20 * magnitude)
                     else:
                         h[8] += magnitude
 
-            rez.append(h)
+            # rez.append(h)
+            rez.extend(h)
     
     return rez
 
-m = genCells(genPixelAvg('img3.jpg',(32,64)))
-genHOG(m)
+hogs = []
+classes = []
+signal_files = os.listdir('./Data/Danger')
+for file in signal_files:
+    hogs.append(genHOG(file, './Data/Danger/'))
+    classes.append(1)
+
+other_files = os.listdir('./Data/Other')
+for file in other_files:
+    hogs.append(genHOG(file, './Data/Other/'))
+    classes.append(0)    
+
+
+# print(hogs)
+# print('\n')
+# print("Klasest:",classes)
+
+clf = SVC(kernel='rbf', random_state=0)
+clf.fit(hogs,classes)
+
+imageToPredict = [genHOG('image_2001.jpg','./Data/Danger/')]
+# print(imageToPredict)
+print("Predict",clf.predict(imageToPredict)) 
+
+
+# m = genCells(genPixelAvg('image_2001.jpg',(32,64)))
+# genHOG(m)
